7b321798e7b2be6c6039fa0c8075b17f83d62f6e
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2016-11-15 23:41:10 -0500

    sorry

--- simple/simple/Message.java
+++ simple/simple/Message.java
@@ -1,14 +1,10 @@
 package simple;
 
 public class Message {
-<<<<<<< HEAD
 
-	byte[] data = new byte[0];
-=======
 	static byte[] dat = new byte[0];
 	byte[] data = dat;
 
->>>>>>> 580d564... freezes
 	enum CellType {
 		not_selected, control, proxy
 	}
@@ -25,37 +21,12 @@ public class Message {
 	public int circID;
 	public Cmd cmd;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 580d564... freezes
 	public Message() {
 		// this.data= new byte[512];
 		// TODO Auto-generated constructor stub
 	}
-<<<<<<< HEAD
-	public boolean isCreate(){return this.cmd==Cmd.create;}
-	public byte[][] createMessage() {
-		// TODO Generate random circID 
-		byte[][] dataArray= new byte[1][512];
-		byte [] data=dataArray[0];
-		data[0]=(byte) (circID>>8);
-		data[1] = (byte) circID;
 
-		if(type == CellType.proxy) {
-			data[2]=(byte) Cmd.relay.ordinal();
-			data[13]=(byte) cmd.ordinal();
-			int len = Math.min(498, this.data.length);
-			data[12]=(byte)len;
-			data[11]=(byte)(len>>8);
-			System.arraycopy(this.data,0,data,14,len);
-
-		} else {
-			data[2]=(byte) cmd.ordinal();
-
-			int len = Math.min(498, this.data.length);
-			System.arraycopy(this.data,0,data,3,len);
-=======
 
 	public boolean isCreate() {
 		return this.cmd == Cmd.create;
@@ -78,17 +49,13 @@ public class Message {
 
 			int len = Math.min(498, this.data.length);
 			System.arraycopy(this.data, 0, data, 3, len);
->>>>>>> 580d564... freezes
 
 		}
 		return data;
 	}
 
-<<<<<<< HEAD
-	public static Message receiveMessage(byte[] data){
-=======
+
 	public static Message receiveMessage(byte[] data) {
->>>>>>> 580d564... freezes
 		Message m = new Message();
 		byte x = data[2];
 		m.cmd = Cmd.values()[x];
@@ -102,36 +69,7 @@ public class Message {
 		}
 		return m;
 
-<<<<<<< HEAD
-	}
-	public static int unsignedToBytes(byte a)
-	{
-		int b = a & 0xFF;
-		return b;
-	}
-	public static int unsignedToBytes(byte[] a,int offset,int len)
-	{
-		int result=0;
-		if (len>4){
-			len =4;
-		}
-		for(int i = 0 ; i < len; i++){
-			result |= unsignedToBytes(a[ offset-i]) << (i * 8);
-		}
-		return result;
-		//	   if (len>4){
-		//	   len =4;
-		//   }
-		//   if (len<0){
-		//	   len =0;
-		//   }
-		//   if (offset+len>=a.length){
-		//	   len =a.length-offset;
-		//   }
-		//   if(offset<0){
-		//	   offset=
-		//   }
-=======
+
 	}
 
 	private static int unsign(byte a) {
@@ -202,7 +140,6 @@ public class Message {
 		}
 
 		return new String(wall, 0, numBytes - 498 + len);
->>>>>>> 580d564... freezes
 	}
 
 }
